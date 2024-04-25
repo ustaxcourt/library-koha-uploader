b92db8993a3691b2b653f8e688607b6c4fbca658
Mike Marcotte <mike.marcotte@gmail.com>
2024-04-24 23:15:21 -0400

    add s3 connection

--- process.d.ts
+++ process.d.ts
@@ -5,5 +5,8 @@ declare namespace NodeJS {
     AZURE_AD_CLIENT_SECRET: string;
     AZURE_AD_CLIENT_ID: string;
     AZURE_AD_TENANT_ID: string;
+    S3_UPLOADS_BUCKET: string;
+    S3_ACCESS_KEY: string;
+    S3_SECRET_KEY: string;
   }
 }
