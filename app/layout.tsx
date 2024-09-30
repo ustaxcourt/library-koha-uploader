fe74af72d27da27ad5d4f33fdac4b6f8aa89b7ac
Mike Marcotte <mike.marcotte@gmail.com>
2024-09-30 13:12:10 -0400

    #10: adjust layouting

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+          <div className="z-10 max-w-5xl w-full justify-between font-mono text-sm lg:flex">
+            {children}
+          </div>
+        </main>
+      </body>
     </html>
   );
 }
